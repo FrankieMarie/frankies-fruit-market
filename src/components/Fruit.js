155e5e89831462cb31cd5954c5b7d34fe500d3a0
FrankieMarie <frankiemarie83@gmail.com>
2018-06-04 14:46:12 -0700

    display all fruits

--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
-const Fruit = () => {
+const Fruit = ({fruit}) => {
+  let {_id, name, price, description, fruitImage} = fruit
+  let desc = {description}
   return (
-    <div>
-
+    <div className="card">
+      <img src={"http://localhost:8000/"+fruitImage} alt="" className="fruit-image"/>
+      <div className="card-content">
+        <h3 className="card-title">{name}</h3>
+        <p>$ {price} / lb</p>
+        <p>{desc.description.substring(0, 160)}...&nbsp;
+          <a href={"/fruit/"+_id} className="read-more">
+          <small>Read more</small>
+          </a>
+        </p>
+      </div>
+      <button className="btn">Add to Cart</button>
     </div>
   )
 }
--- src/components/FruitList.js
+++ src/components/FruitList.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import Fruit from './Fruit'
 
 class FruitList extends Component {
   render() {
+    const listOfFruits = this.props.fruits.map(fruit => <Fruit key={fruit._id} fruit={fruit} />)
     return (
-      <div>
-
+      <div className="fruit-list">
+        {listOfFruits}
       </div>
     )
   }
 }
 
-export default FruitList
+function mapStateToProps(state){
+  return {fruits: state.fruits}
+}
+
+export default connect(mapStateToProps)(FruitList)
