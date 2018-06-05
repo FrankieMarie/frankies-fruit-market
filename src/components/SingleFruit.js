fbaa346edf55dc94ff70857be2f68fcfa5881c84
FrankieMarie <frankiemarie83@gmail.com>
2018-06-04 19:10:58 -0700

    single fruit page

--- src/components/SingleFruit.js
+++ src/components/SingleFruit.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const SingleFruit = () => {
-  return (
-    <div>
-
-    </div>
-  )
+const SingleFruit = ({fruit, history}) => {
+  if(fruit){
+    return (
+      <div className="single-fruit">
+        <button className="btn back" onClick={() => history.goBack()}>Go back</button><br/>
+        <img className="single-fruit-img" src={"http://localhost:8000/"+fruit.fruitImage} alt=""/>
+        <div className="single-fruit-content">
+          <h2>{fruit.name}</h2>
+          <small>$ {fruit.price} / box</small>
+          <p>{fruit.description}</p>
+          <small><i>Each box contains 10-20 pounds of fruit. Amount varies depending on fruit density, size, and type of fruit being packaged. Boxes are shipped through USPS priority mail.</i></small><br/><br/>
+          <button className="btn">Add to Cart</button>
+        </div>
+      </div>
+    )
+  } else {
+    return <div>Loading...</div>
+  }
 }
 
-export default SingleFruit
+const mapStateToProps = (state, props) => ({
+  fruit: state.fruits.filter(fruit => fruit._id === props.match.params.id)[0]
+})
+
+export default connect(mapStateToProps)(SingleFruit)
