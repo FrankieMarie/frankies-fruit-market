928046d9233889cbe3c1d0d14fabb1ef8658170f
FrankieMarie <frankiemarie83@gmail.com>
2018-06-05 10:28:27 -0700

    display fruit images

--- src/components/SingleFruit.js
+++ src/components/SingleFruit.js
@@ -1,28 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-const SingleFruit = ({fruit, history}) => {
-  if(fruit){
-    return (
-      <div className="single-fruit">
-        <button className="btn back" onClick={() => history.goBack()}>Go back</button><br/>
-        <img className="single-fruit-img" src={"http://localhost:8000/"+fruit.fruitImage} alt=""/>
-        <div className="single-fruit-content">
-          <h2>{fruit.name}</h2>
-          <small>$ {fruit.price} / box</small>
-          <p>{fruit.description}</p>
-          <small><i>Each box contains 10-20 pounds of fruit. Amount varies depending on fruit density, size, and type of fruit being packaged. Boxes are shipped through USPS priority mail.</i></small><br/><br/>
-          <button className="btn">Add to Cart</button>
+// const SingleFruit = ({fruit, history}) => {
+//   const imgs = props.images.map(img => <img src={"http://localhost:8000/"+img}/>)
+//   if(fruit){
+//     return (
+//       <div className="single-fruit">
+//         <button className="btn back" onClick={() => history.goBack()}>Go back</button><br/>
+//         <img className="single-fruit-img" src={"http://localhost:8000/"+fruit.fruitImage} alt=""/>
+//         <div className="single-fruit-content">
+//           <h2>{fruit.name}</h2>
+//           <small>$ {fruit.price} / box</small>
+//           <p>{fruit.description}</p>
+//           <small><i>Each box contains 10-20 pounds of fruit. Amount varies depending on fruit density, size, and type of fruit being packaged. Boxes are shipped through USPS priority mail.</i></small><br/><br/>
+//           <button className="btn">Add to Cart</button>
+//         </div>
+//       </div>
+//     )
+//   } else {
+//     return <div>Loading...</div>
+//   }
+// }
+
+class SingleFruit extends Component {
+  render() {
+    let fruit = this.props.fruit
+    const fruitImages = this.props.images.map(img => <img key={img._id} src={"http://localhost:8000/"+img.image} />)
+    //const imgs = this.props.images.map(img => <img src={"http://localhost:8000/uploads/"+img}/>)
+    //console.log(imgs)
+    if(fruit){
+      return (
+        <div className="single-fruit">
+          <button className="btn back" onClick={() => this.props.history.goBack()}>Go back</button><br/>
+          <img className="single-fruit-img" src={"http://localhost:8000/"+fruit.fruitImage} alt=""/>
+          <div className="single-fruit-content">
+            <h2>{fruit.name}</h2>
+            <small>$ {fruit.price} / box</small>
+            <p>{fruit.description}</p>
+            <small><i>Each box contains 10-20 pounds of fruit. Amount varies depending on fruit density, size, and type of fruit being packaged. Boxes are shipped through USPS priority mail.</i></small><br/><br/>
+            <button className="btn">Add to Cart</button>
+            <div className="fruit-imgs">
+              {fruitImages}
+            </div>
+          </div>
         </div>
-      </div>
-    )
-  } else {
-    return <div>Loading...</div>
+      )
+    } else {
+      return <div>Loading...</div>
+    }
   }
 }
 
 const mapStateToProps = (state, props) => ({
-  fruit: state.fruits.filter(fruit => fruit._id === props.match.params.id)[0]
+  fruit: state.fruits.filter(fruit => fruit._id === props.match.params.id)[0],
+  images: state.images.filter(image => image.fruit._id === props.match.params.id)
 })
 
 export default connect(mapStateToProps)(SingleFruit)
