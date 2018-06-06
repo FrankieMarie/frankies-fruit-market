53ecbc0fd6c5c6dc6a95aadbef4e31a1b0b555db
FrankieMarie <frankiemarie83@gmail.com>
2018-06-06 12:08:28 -0700

    cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import CartItem from './CartItem'
+import TopNav from './TopNav'
 
 class Cart extends Component {
   render() {
+    let orderList = this.props.orders.map(item => <CartItem key={item._id} item={item} fruit={item.fruit}/>)
     return (
       <div>
-
+        <TopNav /><br/>
+        <div className="cart-items">
+          {orderList}
+        </div>
+        <hr/>
+        <p>Grand Total: $</p>
+        <button className="btn">Checkout</button>
       </div>
     )
   }
 }
 
-export default Cart
+function mapStateToProps(state) {
+  return {orders: state.orders}
+}
+
+export default connect(mapStateToProps)(Cart)
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const CartItem = () => {
+const CartItem = ({item, fruit}) => {
   return (
     <div>
-
+      <ul>
+        <li className="cart-item-li">
+          <img src={"http://localhost:8000/"+fruit.fruitImage} alt="" className="cart-item-image"/>
+          <div className="cart-item-info">
+            <p>Fruit: {fruit.name}</p>
+            <p>Price: $ {fruit.price} / <small>box</small></p>
+            <p>Quantity: {item.quantity} <small>box</small></p>
+            <p>Item Total: $ {item.quantity * fruit.price}</p>
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
