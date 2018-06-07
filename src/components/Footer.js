e634d442ed7af3fd3c081b7053cc0d4be7d970ca
FrankieMarie <frankiemarie83@gmail.com>
2018-06-07 13:52:32 -0700

    conditionally render add fruit btn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const Footer = () => {
+const Footer = (props) => {
   return (
     <div className="footer">
       <a href='/login' className="admin-login">Admin Login <i className="fas fa-sign-in-alt"></i></a>
-      <a href='/addfruit' className="add-fruit">Add a Fruit <i className="fas fa-plus"></i></a>
+      <a href='/addfruit' className="add-fruit">{props.admin.isAuthed ? 'Add a Fruit +' : null} </a>
     </div>
   )
 }
 
-export default Footer
+const mapStateToProps = (state) => ({
+  admin: state.admin
+})
+
+export default connect(mapStateToProps)(Footer)
